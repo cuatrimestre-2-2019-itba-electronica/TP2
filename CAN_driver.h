96729709d701154c4f4ba0cc99adb41c701bb99b
Matias <mpierdominici@itba.edu.ar>
2019-10-06 20:35:43 -0300

    no se usa

--- CAN_driver.h
+++
@@ -1,7 +0,0 @@
-void CAN_RESET(void);
-void CAN_BIT_MODIFY(char address, char mask, char data);
-void CAN_WRITE(char address, char * buffer, int bufflen);
-void CAN_READ(char address, char * buffer, int bufflen);
-void CAN_RTS(char address, char * buffer, int bufflen);
-void init_CAN(int ID);
-void send_CAN(int ID,char * buffer, int bufflen);
